26a3615ba5462472c1196e370fea56b947517562
SirAardvark <69832032+SirAardvark@users.noreply.github.com>
2023-09-16 19:36:30 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReQuest - IOD Capstone Project - Justin Aavik
+
+ReQuest is a ticket management system.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>ReQuest</h1>
+            <p>IOD Capstone Project - Justin Aavik</p>
+        </>
+    );
+}
+
+export default App;
